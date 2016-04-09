23abf2d539b5df0c070fb25cb40dbbdaf95a75a7
Harbardr <jfern.paris@gmail.com>
2016-04-09 18:43:08 +0200

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,28 +1,13 @@
-# Servo Control
+# !/usr/bin/python
 import time
-def set(property, value):
-	try:
-		f = open("/sys/class/rpi-pwm/pwm0/" + property, 'w')
-		f.write(value)
-		f.close()	
-	except:
-		print("Error writing to: " + property + " value: " + value)
- 
- 
-def setServo(angle):
-	set("servo", str(angle))
-	
-set("delayed", "0")
-set("mode", "servo")
-set("servo_max", "180")
-set("active", "1")
- 
-delay_period = 0.01
- 
-while True:
-	for angle in range(0, 180):
-		setServo(angle)
-		time.sleep(delay_period)
-	for angle in range(0, 180):
-		setServo(180 - angle)
-		time.sleep(delay_period)
+import RPi.GPIO as GPIO
+from RPIO import PWM
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17,GPIO.OUT)
+PWM.setup()
+PWM.init_channel(0)
+while True : 
+	for i in range(100,201):
+		PWM.add_channel_pulse(0, 17, 0, i)
+		time.sleep(0.05)
