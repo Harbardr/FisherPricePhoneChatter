46aaccd88ea8751d92b6f3c2a237568629bb73c2
Harbardr <jfern.paris@gmail.com>
2016-04-09 18:34:46 +0200

    Update servo2.py

--- servo2.py
+++ servo2.py
@@ -1,14 +1,30 @@
-#from Tkinter import *
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(18, GPIO.OUT)
-pwm = GPIO.PWM(18, 50)
-pwm.start(5)
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
 
-#while True:
-#   GPIO.output(18, GPIO.HIGH)  
-#   time.sleep(0.0015)  
-#   GPIO.output(18, GPIO.LOW)  
-#   time.sleep(0.0185)
+p = GPIO.PWM(servoPIN, 50) # GPIO 18 als PWM mit 50Hz
+p.start(2.5) # Initialisierung
+try:
+    while True:
+        p.ChangeDutyCycle(5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(7.5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(10)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(12.5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(10)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(7.5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(2.5)
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
