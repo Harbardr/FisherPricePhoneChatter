3bf619730f8aa3abb2ff427d25e5eeef8e572144
Harbardr <jfern.paris@gmail.com>
2016-04-09 18:30:39 +0200

    Update servo2.py

--- servo2.py
+++ servo2.py
@@ -2,13 +2,13 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(18, GPIO.OUT)
-pwm = GPIO.PWM(18, 100)
+pwm = GPIO.PWM(18, 50)
 pwm.start(5)
 
-while True:
-   GPIO.output(18, GPIO.HIGH)  
-   time.sleep(0.0015)  
-   GPIO.output(18, GPIO.LOW)  
-   time.sleep(0.0185)
+#while True:
+#   GPIO.output(18, GPIO.HIGH)  
+#   time.sleep(0.0015)  
+#   GPIO.output(18, GPIO.LOW)  
+#   time.sleep(0.0185)
