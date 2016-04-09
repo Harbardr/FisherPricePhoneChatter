984aa7484fca7d4cbf295aebc577d235369a9d9b
Harbardr <jfern.paris@gmail.com>
2016-04-09 22:00:20 +0200

    Update servo.py

--- servo.py
+++ servo.py
@@ -4,9 +4,9 @@ import RPi.GPIO as GPIO
 from RPIO import PWM
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17,GPIO.OUT)
+GPIO.setup(22,GPIO.OUT)
 PWM.setup()
-PWM.init_channel(1)
+PWM.init_channel(0)
 
 def eyes(wink=3):
 	for i in (1,wink):
@@ -20,7 +20,7 @@ def eyes(wink=3):
 if __name__ == '__main__':
 	while True : 
 		#for i in range(100,201):
-		PWM.add_channel_pulse(1, 17, 0, 100)
+		PWM.add_channel_pulse(0, 17, 0, 100)
 		time.sleep(0.5)
-		PWM.add_channel_pulse(1, 17, 0, 200)
+		PWM.add_channel_pulse(0, 17, 0, 200)
 		time.sleep(1)
