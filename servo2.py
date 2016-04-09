3c6afeab23f730f86dc1de92099c91bec343a401
Harbardr <jfern.paris@gmail.com>
2016-04-09 18:21:54 +0200

    Update servo2.py

--- servo2.py
+++ servo2.py
@@ -1,4 +1,4 @@
-from Tkinter import *
+#from Tkinter import *
 import RPi.GPIO as GPIO
 import time
 
@@ -7,22 +7,8 @@ GPIO.setup(18, GPIO.OUT)
 pwm = GPIO.PWM(18, 100)
 pwm.start(5)
 
-class App:
-
-    def __init__(self, master):
-        frame = Frame(master)
-        frame.pack()
-        scale = Scale(frame, from_=0, to=180,
-              orient=HORIZONTAL, command=self.update)
-        scale.grid(row=0)
-
-
-    def update(self, angle):
-        duty = float(angle) / 10.0 + 2.5
-        pwm.ChangeDutyCycle(duty)
-
-root = Tk()
-root.wm_title('Servo Control')
-app = App(root)
-root.geometry("200x50+0+0")
-root.mainloop()
+while True:
+   GPIO.output(pin, GPIO.HIGH)  
+   time.sleep(0.0015)  
+   GPIO.output(pin, GPIO.LOW)  
+   time.sleep(0.0185)
