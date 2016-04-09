0386072e9af5cb1bed7ac2732d439d9f7d4626ba
Harbardr <jfern.paris@gmail.com>
2016-04-09 18:36:34 +0200

    Update servo2.py

--- servo2.py
+++ servo2.py
@@ -10,8 +10,10 @@ p.start(2.5) # Initialisierung
 try:
     while True:
         p.ChangeDutyCycle(5)
+        print("5")
         time.sleep(0.5)
         p.ChangeDutyCycle(7.5)
+        print("7.5")
         time.sleep(0.5)
         p.ChangeDutyCycle(10)
         time.sleep(0.5)
