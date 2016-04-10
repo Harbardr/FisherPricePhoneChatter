31e1c78769f1c1f245ef415232ff7795e16d9d2d
Harbardr <jfern.paris@gmail.com>
2016-04-10 15:50:18 +0200

    Update rotary.py

--- rotary.py
+++ rotary.py
@@ -119,7 +119,8 @@ def numbers(number, longNumber=False):
         print ("-  888888  -")
         print ("------------")
 
-
+phoneNumber = ""
+sequenceNumber = ""
 longNumber = False
 while True:
     delta = encoder.get_delta()
@@ -135,32 +136,43 @@ while True:
         if select_state <= 91 and select_state > 81:
             print ("rotate %d = (9)" % select_state)
             numbers(9,longNumber)
+            sequenceNumber = "9"
         elif select_state <= 81 and select_state > 71:
             print ("rotate %d = (8)" % select_state)
             numbers(8,longNumber)
+            sequenceNumber = "8"
         elif select_state <= 71 and select_state > 61:
             print ("rotate %d = (7)" % select_state)
             numbers(7,longNumber)
+            sequenceNumber = "7"
         elif select_state <= 61 and select_state > 51:
             print ("rotate %d = (6)" % select_state)
             numbers(6,longNumber)
+            sequenceNumber = "6"
         elif select_state <= 51 and select_state > 41:
             print ("rotate %d = (5)" % select_state)
             numbers(5,longNumber)
+            sequenceNumber = "5"
         elif select_state <= 41 and select_state > 31:
             print ("rotate %d = (4)" % select_state)
             numbers(4,longNumber)
+            sequenceNumber = "4"
         elif select_state <= 31 and select_state > 21:
             print ("rotate %d = (3)" % select_state)
             numbers(3,longNumber)
+            sequenceNumber = "3"
         elif select_state <= 21 and select_state > 11:
             print ("rotate %d = (2)" % select_state)
             numbers(2,longNumber)
+            sequenceNumber = "2"
         elif select_state <= 11 and select_state > 0:
             print ("rotate %d = (1)" % select_state)
             numbers(1,longNumber)
+            sequenceNumber = "1"
         if return_state > 0 and return_state < 5:
             print ("== REINITIALISATION ==")
+        phoneNumber = phoneNumber + sequenceNumber
+        sequenceNumber=""
         select_state=0
         return_state=0
         
@@ -168,3 +180,6 @@ while True:
     if sw_state != last_state:
         print ("switch %d" % sw_state)
         last_state = sw_state
+        for i in phoneNumber:
+            numbers(integer(i),longNumber)+"   "
+        phoneNumber = ""
