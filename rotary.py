1f5e1cd5b60b319bbc540f527f51e82cf798dfc8
Harbardr <jfern.paris@gmail.com>
2016-04-10 17:37:12 +0200

    Update rotary.py

--- rotary.py
+++ rotary.py
@@ -13,10 +13,6 @@ SW_PIN = 8
 encoder = gaugette.rotary_encoder.RotaryEncoder.Worker(A_PIN, B_PIN)
 encoder.start()
 switch = gaugette.switch.Switch(SW_PIN)
-last_state = None
-
-select_state = 0
-return_state = 0
 
 def numbers(number, longNumber=False):
     if number == "9":
@@ -120,69 +116,79 @@ def numbers(number, longNumber=False):
         print ("-  888888  -")
         print ("------------")
 
-phoneNumber = ""
-sequenceNumber = ""
-longNumber = False
-while True:
-    delta = encoder.get_delta()
-    #if delta!=0:
-        #print ("rotate %d" % delta)
-    if delta<0:
-        select_state+=delta
-        #print ("rotate %d" % select_state)
-    if delta>0:
-        return_state+=delta
-    if return_state > 10:
-        select_state = abs(select_state)
-        if select_state <= 91 and select_state > 81:
-            print ("rotate %d = (9)" % select_state)
-            numbers("9",longNumber)
-            sequenceNumber = "9"
-        elif select_state <= 81 and select_state > 71:
-            print ("rotate %d = (8)" % select_state)
-            numbers("8",longNumber)
-            sequenceNumber = "8"
-        elif select_state <= 71 and select_state > 61:
-            print ("rotate %d = (7)" % select_state)
-            numbers("7",longNumber)
-            sequenceNumber = "7"
-        elif select_state <= 61 and select_state > 51:
-            print ("rotate %d = (6)" % select_state)
-            numbers("6",longNumber)
-            sequenceNumber = "6"
-        elif select_state <= 51 and select_state > 41:
-            print ("rotate %d = (5)" % select_state)
-            numbers("5",longNumber)
-            sequenceNumber = "5"
-        elif select_state <= 41 and select_state > 31:
-            print ("rotate %d = (4)" % select_state)
-            numbers("4",longNumber)
-            sequenceNumber = "4"
-        elif select_state <= 31 and select_state > 21:
-            print ("rotate %d = (3)" % select_state)
-            numbers("3",longNumber)
-            sequenceNumber = "3"
-        elif select_state <= 21 and select_state > 11:
-            print ("rotate %d = (2)" % select_state)
-            numbers("2",longNumber)
-            sequenceNumber = "2"
-        elif select_state <= 11 and select_state > 0:
-            print ("rotate %d = (1)" % select_state)
-            numbers("1",longNumber)
-            sequenceNumber = "1"
-        if return_state > 0 and return_state < 5:
-            print ("== REINITIALISATION ==")
-        phoneNumber = phoneNumber + sequenceNumber
-        sequenceNumber=""
-        select_state=0
-        return_state=0
-        
-    sw_state = switch.get_state()
-    if sw_state != last_state:
-        print ("switch %d" % sw_state)
-        time.sleep(2)
-        last_state = sw_state
-        for i in phoneNumber:
-            numbers(i,longNumber)
-            time.sleep(0.5)
-        phoneNumber = ""
+
+def rotary():
+    last_state = None
+    select_state = 0
+    return_state = 0
+    phoneNumber = ""
+    sequenceNumber = ""
+    longNumber = False
+    while True:
+        delta = encoder.get_delta()
+        #if delta!=0:
+            #print ("rotate %d" % delta)
+        if delta<0:
+            select_state+=delta
+            #print ("rotate %d" % select_state)
+        if delta>0:
+            return_state+=delta
+        if return_state > 10:
+            select_state = abs(select_state)
+            if select_state <= 91 and select_state > 81:
+                print ("rotate %d = (9)" % select_state)
+                numbers("9",longNumber)
+                sequenceNumber = "9"
+            elif select_state <= 81 and select_state > 71:
+                print ("rotate %d = (8)" % select_state)
+                numbers("8",longNumber)
+                sequenceNumber = "8"
+            elif select_state <= 71 and select_state > 61:
+                print ("rotate %d = (7)" % select_state)
+                numbers("7",longNumber)
+                sequenceNumber = "7"
+            elif select_state <= 61 and select_state > 51:
+                print ("rotate %d = (6)" % select_state)
+                numbers("6",longNumber)
+                sequenceNumber = "6"
+            elif select_state <= 51 and select_state > 41:
+                print ("rotate %d = (5)" % select_state)
+                numbers("5",longNumber)
+                sequenceNumber = "5"
+            elif select_state <= 41 and select_state > 31:
+                print ("rotate %d = (4)" % select_state)
+                numbers("4",longNumber)
+                sequenceNumber = "4"
+            elif select_state <= 31 and select_state > 21:
+                print ("rotate %d = (3)" % select_state)
+                numbers("3",longNumber)
+                sequenceNumber = "3"
+            elif select_state <= 21 and select_state > 11:
+                print ("rotate %d = (2)" % select_state)
+                numbers("2",longNumber)
+                sequenceNumber = "2"
+            elif select_state <= 11 and select_state > 0:
+                print ("rotate %d = (1)" % select_state)
+                numbers("1",longNumber)
+                sequenceNumber = "1"
+            if return_state > 0 and return_state < 5:
+                print ("== REINITIALISATION ==")
+            phoneNumber = phoneNumber + sequenceNumber
+            sequenceNumber=""
+            select_state=0
+            return_state=0
+            
+        sw_state = switch.get_state()
+        if sw_state != last_state:
+            print ("switch %d" % sw_state)
+            time.sleep(2)
+            last_state = sw_state
+            if len(phoneNumber)>1:
+                for i in phoneNumber:
+                    numbers(i,longNumber)
+                    time.sleep(0.5)
+                phoneNumber = ""
+            else:
+                return phoneNumber
+if "__main__"
+    rotary()
