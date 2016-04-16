8efe95faaca27334f708686a43cca5cc8cfaecb2
Harbardr <jfern.paris@gmail.com>
2016-04-16 23:53:17 +0200

    Update rotary.py

--- rotary.py
+++ rotary.py
@@ -119,6 +119,7 @@ def rotary():
     encoder.start()
     switch = gaugette.switch.Switch(SW_PIN)
     
+    switch_state = 0
     last_state = None
     select_state = 0
     return_state = 0
@@ -181,9 +182,9 @@ def rotary():
             return_state=0
             
         sw_state = switch.get_state()
-        #print ("switch %d - %d" % sw_state, last_state)
-        print (sw_state)
-        print(last_state)
+        print ("switch %d - %d" % sw_state, last_state)
+        #print (sw_state)
+        #print(last_state)
         if (sw_state != last_state and last_state != None):
             print ("switch %d" % sw_state)
             last_state = sw_state
