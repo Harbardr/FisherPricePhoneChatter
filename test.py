666906cb7e3f65670ad14a441d9d916b4343fed5
Harbardr <jfern.paris@gmail.com>
2016-07-02 22:28:00 +0200

    Update test.py

--- test.py
+++ test.py
@@ -103,7 +103,7 @@ def main(stdscr):
         
         rotaryAction = rotaryPhone()
         event = rotaryAction.rotary()
-        if(event!=""):print(event)
+        if(event!=None):print(event)
         
         if event == ord("q"): break
         elif event ==  ord("1"):
