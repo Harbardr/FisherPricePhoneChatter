ffaff28696e5eac95ba92debd27a1e9ccf9bcf19
Harbardr <jfern.paris@gmail.com>
2016-07-07 21:32:19 +0200

    Update test.py

--- test.py
+++ test.py
@@ -28,10 +28,10 @@ import i8n
 #End TO Check
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-r", "--rotary", nargs='?', action="store_true", help="Use the encoder")
-parser.add_argument("-k", "--keyboard", nargs='?', action="store_true", help="Use the keyboard")
-parser.add_argument("-fr", "--french", nargs='?', action="store_true", help="French language")
-parser.add_argument("-en", "--english", nargs='?', action="store_true", help="English language")
+parser.add_argument("-r", "--rotary", action="store_true", help="Use the encoder")
+parser.add_argument("-k", "--keyboard", action="store_true", help="Use the keyboard")
+parser.add_argument("-fr", "--french", action="store_true", help="French language")
+parser.add_argument("-en", "--english", action="store_true", help="English language")
 args = parser.parse_args()
 
 print args
