45c8f6586f80c5736e8a60d68c78883b92114b54
Harbardr <root@midgard.10.255.0.1>
2016-10-23 00:32:37 +0200

    test

--- gmail.py
+++ gmail.py
@@ -136,3 +136,17 @@ def GetMessage(service, user_id, msg_id, snippetMessage=True):
     except errors.HttpError, error:
         print('An error occurred: %s' % error)
 
+def DeleteMessage(service, user_id, msg_id):
+    """Delete a Message.
+
+    Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    msg_id: ID of Message to delete.
+    """
+    try:
+        service.users().messages().delete(userId=user_id, id=msg_id).execute()
+        print 'Message with id: %s deleted successfully.' % msg_id
+    except errors.HttpError, error:
+        print 'An error occurred: %s' % error
