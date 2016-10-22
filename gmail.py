5631c27aad2abdab60b5ec30f7cfb3aff7408d2f
Harbardr <root@midgard.10.255.0.1>
2016-10-23 00:45:18 +0200

    test

--- gmail.py
+++ gmail.py
@@ -17,7 +17,9 @@ from oauth2client import tools
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/gmail-python-quickstart.json
 HOME = "/home/pi/"
-SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+# SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+SCOPES = 'https://www.googleapis.com/auth/gmail.modify'
+
 CLIENT_SECRET_FILE = '/home/pi/client_secret.json'
 APPLICATION_NAME = 'FisherPriceChatter'
 
