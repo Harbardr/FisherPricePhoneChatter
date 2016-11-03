b5146f7f1f3127442954fdc598eda29d72925b3f
Harbardr <root@midgard.10.255.0.1>
2016-11-03 20:59:52 +0100

    test

--- gmail.py
+++ gmail.py
@@ -155,5 +155,26 @@ def DeleteMessage(service, user_id, msg_id):
         print('An error occurred: %s' % error)
 
 
+def main():
+    """Shows basic usage of the Gmail API.
+
+    Creates a Gmail API service object and outputs a list of label names
+    of the user's Gmail account.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+
+    if not labels:
+        print('No labels found.')
+    else:
+        print('Labels:')
+        for label in labels:
+            print(label['name'])
+
+
 if __name__ == '__main__':
     main()
