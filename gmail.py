da458f07f952c9c5da85aa8f3cc4a508df402c66
Harbardr <root@midgard.10.255.0.1>
2016-11-03 20:31:41 +0100

    test

--- gmail.py
+++ gmail.py
@@ -18,6 +18,7 @@ from oauth2client import tools
 # at ~/.credentials/gmail-python-quickstart.json
 HOME = "/home/pi/"
 # SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+# https://developers.google.com/gmail/api/auth/scopes
 SCOPES = 'https://mail.google.com/'
 CLIENT_SECRET_FILE = '/home/pi/client_secret.json'
 APPLICATION_NAME = 'FisherPriceChatter'
